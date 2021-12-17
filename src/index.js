43d3f232dc903ac288ec34ac00bbfc9a24d7084d
David Hermoso <juandavidhermoso@gmail.com>
2021-12-17 10:53:31 +0100

    Add routing and pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Destinations from "./pages/Destinations";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+import Home from "./pages/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="destinations" element={<Destinations/>}/>
+        <Route path="crew" element={<Crew/>}/>
+        <Route path="technology" element={<Technology/>}/>
+        <Route
+          path="*"
+          element={
+            <Navigate to="/"/>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
